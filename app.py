bd742883d8c7d9057d8ae419013624391ce76fc6
predat-r <harisnaeem612@outlook.com>
2025-06-17 21:37:25 +0500

    port fix

--- app.py
+++ app.py
@@ -292,4 +292,6 @@ def search():
     return render_template('search.html', questions=questions, query=query, current_user=get_current_user())
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
